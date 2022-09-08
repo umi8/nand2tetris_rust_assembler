4e37b551859d1978baff4a6e86e93c66cad55709
Hiromi Furukawa <umi8@users.noreply.github.com>
2022-09-09 08:57:01 +0900

    feat: impl CommandType

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{BufRead, BufReader, Write};
+
+fn main() -> std::io::Result<()> {
+    let asm = File::open("Prog.asm")?;
+    let reader = BufReader::new(asm);
+
+    let mut file = File::create("Prog.hack")?;
+    for line in reader.lines() {
+        if let Ok(command) = line {
+            match CommandType::from(command) {
+                CommandType::A => file.write(b"A\n")?,
+                CommandType::C => file.write(b"C\n")?,
+            };
+        };
+    }
+    Ok(())
+}
+
+enum CommandType {
+    A,
+    C,
+}
+
+impl CommandType {
+    fn from(command: String) -> CommandType {
+        return if command.starts_with("@") {
+            CommandType::A
+        } else {
+            CommandType::C
+        }
+    }
 }
