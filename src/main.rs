53ed3d3b64d50e043b4e27aec7fb20b893681697
Hiromi Furukawa <umi8@users.noreply.github.com>
2022-09-16 08:17:34 +0900

    feat: create parser module

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,32 @@
 use std::fs::File;
-use std::io::{BufRead, BufReader, Write};
+use std::io::Write;
 
-use regex::Regex;
+use crate::parser::CommandType;
 
 mod code;
+mod parser;
 
 fn main() -> std::io::Result<()> {
-    let asm = File::open("Prog.asm")?;
-    let reader = BufReader::new(asm);
-
     let mut file = File::create("Prog.hack")?;
 
-    for line in reader.lines() {
-        if let Ok(command) = line {
-            if command.starts_with("//") {
-                continue;
+    let mut parser = match parser::parser::Parser::new() {
+        Ok(parser) => parser,
+        Err(why) => panic!("couldn't parse: {}", why)
+    };
+
+    while parser.has_more_commands() {
+        match parser.command_type() {
+            CommandType::A => {
+                let num = parser.symbol().parse::<i32>().unwrap();
+                writeln!(&mut file, "{:016b}", num)?
             }
-
-            match CommandType::from(&command) {
-                CommandType::A => {
-                    // @をとる
-                    let value = &command.replace("@", "");
-                    // 10進数の数値に型変換する
-                    let num = value.parse::<i32>().unwrap();
-                    // 16bitの2進数で出力する
-                    writeln!(&mut file, "{:016b}", num)?
-                }
-                CommandType::C => {
-                    let re = Regex::new(r"^(.{1,3}=)*(.*?)(;.{3})*$").unwrap();
-                    let caps = re.captures(&command).unwrap();
-
-                    let comp_mnemonic = caps.get(2).map_or("", |m| m.as_str());
-                    let comp_code = code::comp(&comp_mnemonic);
-
-                    let dest_mnemonic = caps.get(1).map_or("", |m| m.as_str()).replace("=", "");
-                    let dest_code = code::dest(&dest_mnemonic);
-
-                    let jump_mnemonic = caps.get(3).map_or("", |m| m.as_str()).replace(";", "");
-                    let jump_code = code::jump(&jump_mnemonic);
-                    writeln!(&mut file, "{}{}{}{}", "111", comp_code, dest_code, jump_code)?
-                }
-            };
-        };
-    }
-    Ok(())
-}
-
-enum CommandType {
-    A,
-    C,
-}
-
-impl CommandType {
-    fn from(command: &String) -> CommandType {
-        return if command.starts_with("@") {
-            CommandType::A
-        } else {
-            CommandType::C
+            CommandType::C => {
+                let dest_code = code::dest(&parser.dest()).to_string();
+                let comp_code = code::comp(&parser.comp()).to_string();
+                let jump_code = code::jump(&parser.jump()).to_string();
+                writeln!(&mut file, "{}{}{}{}", "111", comp_code, dest_code, jump_code)?
+            }
         }
     }
+    Ok(())
 }
---
+++ src/parser.rs
@@ -0,0 +1,98 @@
+pub mod parser {
+    use std::fs::File;
+    use std::io::{BufRead, BufReader};
+
+    use regex::Regex;
+
+    use crate::parser::CommandType;
+
+    pub struct Parser {
+        reader: BufReader<File>,
+        command: String,
+    }
+
+    impl Parser {
+        pub fn new() -> Result<Self, &'static str> {
+            let asm = File::open("Prog.asm");
+
+            match asm {
+                Ok(file) => {
+                    let reader = BufReader::new(file);
+                    Ok(Parser {
+                        reader,
+                        command: "".parse().unwrap(),
+                    })
+                }
+                Err(_) => Err("file error"),
+            }
+        }
+
+        pub fn has_more_commands(&mut self) -> bool {
+            loop {
+                let mut buf = String::new();
+                return match &self.reader.read_line(&mut buf) {
+                    Ok(0) => false,
+                    Ok(_) => {
+                        if buf.starts_with("//") {
+                            continue;
+                        }
+                        if buf.ends_with("\n") {
+                            buf.pop();
+                        }
+                        self.command = buf;
+                        true
+                    }
+                    Err(_) => false
+                }
+            }
+        }
+
+        pub fn command_type(&self) -> CommandType {
+            CommandType::from(&self.command)
+        }
+
+        pub fn symbol(&self) -> String {
+            // @をとる
+            let value = &self.command.replace("@", "");
+            // 10進数の数値に型変換する
+            let num = value.parse::<i32>().unwrap();
+            num.to_string()
+        }
+
+        pub fn dest(&self) -> String {
+            let re = Regex::new(r"^(.{1,3}=)*(.*?)(;.{3})*$").unwrap();
+            let caps = re.captures(&self.command).unwrap();
+            let dest_mnemonic = caps.get(1).map_or("", |m| m.as_str()).replace("=", "");
+            dest_mnemonic
+        }
+
+        pub fn comp(&self) -> String {
+            let re = Regex::new(r"^(.{1,3}=)*(.*?)(;.{3})*$").unwrap();
+            let caps = re.captures(&self.command).unwrap();
+            let comp_mnemonic = caps.get(2).map_or("", |m| m.as_str());
+            comp_mnemonic.to_string()
+        }
+
+        pub fn jump(&self) -> String {
+            let re = Regex::new(r"^(.{1,3}=)*(.*?)(;.{3})*$").unwrap();
+            let caps = re.captures(&self.command).unwrap();
+            let jump_mnemonic = caps.get(3).map_or("", |m| m.as_str()).replace(";", "");
+            jump_mnemonic
+        }
+    }
+}
+
+pub enum CommandType {
+    A,
+    C,
+}
+
+impl CommandType {
+    fn from(command: &String) -> CommandType {
+        return if command.starts_with("@") {
+            CommandType::A
+        } else {
+            CommandType::C
+        }
+    }
+}
