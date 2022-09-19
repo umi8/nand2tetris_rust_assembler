9c624faf03eff3abe51ccc4a87da4141e2c2925a
Hiromi Furukawa <umi8@users.noreply.github.com>
2022-09-19 11:10:03 +0900

    test: command type a and c

--- src/command_type.rs
+++ src/command_type.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, PartialEq)]
 pub enum CommandType {
     A,
     C,
@@ -10,5 +11,20 @@ impl CommandType {
         } else {
             CommandType::C
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::CommandType;
+
+    #[test]
+    fn return_type_a() {
+        assert_eq!(CommandType::A, CommandType::from(&String::from("@2")))
+    }
+
+    #[test]
+    fn return_type_c() {
+        assert_eq!(CommandType::C, CommandType::from(&String::from("M=D")))
     }
 }
